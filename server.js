2f4062f0956eeea7b232bc99bb6625e0a440dc20
Mukesh <dvrmukesh2002@gmail.com>
2025-01-22 14:09:09 +0530

    'ne

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://euphonious-cat-31efe0.netlify.app/",
+    origin: "https://euphonious-cat-31efe0.netlify.app",
     credentials: true,
   })
 );
